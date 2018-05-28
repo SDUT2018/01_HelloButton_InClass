2e47a53a9db3b21b5fb4efb8521d2c3f8fd72be1
David Fisher <fisherds@rose-hulman.edu>
2018-05-28 11:25:09 +0800

    Capture the TextView (now without a crash)

--- app/src/main/java/edu/rosehulman/fisherds/hellobutton/MainActivity.java
+++ app/src/main/java/edu/rosehulman/fisherds/hellobutton/MainActivity.java
@@ -1,13 +1,27 @@
 package edu.rosehulman.fisherds.hellobutton;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+  private TextView mMessageTextView;
+  private static final String TAG = "HelloButton";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    mMessageTextView = findViewById(R.id.message_text_view);
+
+//    mMessageTextView.setText("Quick test");
+//    Log.d(TAG, "This is a console log for developers.");
+
+    // Cause a crash on purpose
+//    mMessageTextView = findViewById(0);
+//    mMessageTextView.setText("This will crash!");
+
   }
 }
