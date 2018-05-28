84d2664232bdca911da296ea52f0e1bcd315e5a8
David Fisher <fisherds@rose-hulman.edu>
2018-05-28 11:32:24 +0800

    Finished connecting the View to the Controller

--- app/src/main/java/edu/rosehulman/fisherds/hellobutton/MainActivity.java
+++ app/src/main/java/edu/rosehulman/fisherds/hellobutton/MainActivity.java
@@ -2,6 +2,8 @@ package edu.rosehulman.fisherds.hellobutton;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,15 +15,25 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
     mMessageTextView = findViewById(R.id.message_text_view);
-
 //    mMessageTextView.setText("Quick test");
 //    Log.d(TAG, "This is a console log for developers.");
 
     // Cause a crash on purpose
 //    mMessageTextView = findViewById(0);
 //    mMessageTextView.setText("This will crash!");
-
   }
+
+  public void pressedDecrement(View view) {
+    Log.d(TAG, "You pressed the decrement button");
+  }
+
+  public void pressedReset(View view) {
+    Log.d(TAG, "You pressed the reset button");
+  }
+
+  public void pressedIncrement(View view) {
+    Log.d(TAG, "You pressed the increment button");
+  }
+
 }
